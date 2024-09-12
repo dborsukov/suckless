6cd7ae26917fb60619521af3e524b1f42cb497a2
Denis Borsukov <borsukov@proton.me>
2024-09-12 07:22:34 +0300

    dwmblocks: disk module

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -2,7 +2,8 @@ static unsigned int delimLen = 5;
 static char delim[] = " "; // NULL character ('\0') means no delimiter
 
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",            "sb-volume",		0,		        10                       },
-	{"",            "sb-clock",	        5,		        0                        },
+	/*Icon*/  /*Command*/		/*Upd Interval*/	/*Upd Signal*/
+	{"",      "sb-disk /home",	10,		        0		},
+	{"",      "sb-volume",		0,		        10              },
+	{"",      "sb-clock",	        5,		        0               },
 };
